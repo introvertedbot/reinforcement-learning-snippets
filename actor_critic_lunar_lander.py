835c81af1592625dbfb330cf45ad4ef89206fa6b
introvertedbot <introvertedbot@protonmail.com>
2021-01-28 07:05:27 +0530

    Finished actor critic lunar lander

--- actor_critic_lunar_lander.py
+++ actor_critic_lunar_lander.py
@@ -10,6 +10,8 @@ import torch as T
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import gym
+import matplotlib.pyplot as plt
 
 class ActorCriticNetwork(nn.Module):
     def __init__(self, lr, input_dims, n_actions, fc1_dims=256, fc2_dims=256):
@@ -71,3 +73,43 @@ class Agent():
         self.actor_critic.optimizer.step()
 
 
+def plot_learning_curve(x, scores, figure_file):
+    running_avg = np.zeros(len(scores))
+    for i in range(len(running_avg)):
+        running_avg[i] = np.mean(scores[max(0, i-100):(i+1)])
+    plt.plot(x, running_avg)
+    plt.title('Running average of previous 100 scores')
+    plt.savefig(figure_file)
+
+
+
+if __name__ == '__main__':
+    env = gym.make('LunarLander-v2')
+    agent = Agent(gamma=0.99, lr=5e-6, input_dims=[8], n_actions=4,
+                  fc1_dims=2048, fc2_dims=1536)
+    n_games = 3000
+
+    fname = 'ACTOR_CRITIC_' + 'lunar_lander_' + str(agent.fc1_dims) + \
+            '_fc1_dims_' + str(agent.fc2_dims) + '_fc2_dims_lr' + str(agent.lr) +\
+            '_' + str(n_games) + 'games'
+    figure_file = 'plots/' + fname + '.png'
+
+    scores = []
+    for i in range(n_games):
+        done = False
+        observation = env.reset()
+        score = 0
+        while not done:
+            action = agent.choose_action(observation)
+            observation_, reward, done, info = env.step(action)
+            score += reward
+            agent.learn(observation, reward, observation_, done)
+            observation = observation_
+        scores.append(score)
+
+        avg_score = np.mean(scores[-100:])
+        print('episode ', i, 'score %.1f' % score,
+                'average score %.1f' % avg_score)
+
+    x = [i+1 for i in range(n_games)]
+    plot_learning_curve(x, scores, figure_file)
