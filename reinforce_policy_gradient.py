a373a6850362798dfaf3ec74f5b96219214dc159
introvertedbot <introvertedbot@protonmail.com>
2021-02-01 04:50:09 +0530

    Finished policy netowork

---
+++ reinforce_policy_gradient.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 04:40:55 2021
+
+@author: IntrovertedBot
+"""
+
+import numpy as np
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class PolicyNetwork(nn.Module):
+    def __init__(self, lr, input_dims, n_actions):
+        super(PolicyNetwork, self).__init__()
+        self.fc1 = nn.Linear(*input_dims, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, n_actions)
+        self.optimizer = optim.Adam(self.parameters(), lr = lr)
+        
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+        
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        
+        return x
+    
