70b53c3a96a76566d85f39fa0d467d7c26709b9d
introvertedbot <introvertedbot@protonmail.com>
2021-02-01 05:11:38 +0530

    Finished policy gradient agent

--- reinforce_policy_gradient.py
+++ reinforce_policy_gradient.py
@@ -28,4 +28,50 @@ class PolicyNetwork(nn.Module):
         x = self.fc3(x)
         
         return x
+
+class PolicyGradientAgent():
+    def __init__(self, lr, input_dims, gamma=0.99, n_actions=4):
+        self.gamma = gamma
+        self.lr = lr
+        self.reward_memory = []
+        self.action_memory = []
+        
+        self.policy = PolicyNetwork(self.lr, input_dims, n_actions)
+        
+    def choose_action(self, observation):
+        state = T.Tensor([observation]).to(self.policy.device)
+        probabilities = F.softmax(self.policy.forward(state))
+        action_probs = T.distributions.Categorical(probabilities)
+        action = action_probs.sample()
+        log_probs = action_probs.log_prob(action)
+        self.action_memory.append(log_probs)
+        
+        return action.item()
     
+    def store_reward(self, reward):
+        self.reward_memory.append(reward)
+    
+    def learn(self):
+        self.policy.optimizer.zero_grad()
+        
+        # G_t = R_t+1 + gamma * R_t+2 + gamma * R_t+3
+        # G_t = sum from k=0 to k=T {gamma**k * R_t+k+1}
+        G = np.zeros_like(self.reward_memory, dtype=np.float64)
+        for t in range(len(self.reward_memory)):
+            G_sum = 0
+            discount = 1
+            for k in range(t, len(self.reward_memory)):
+                G_sum += self.reward_memory[k] * discount
+                discount *= self.gamma
+            G[t] = G_sum
+        G = T.tensor(G, dtype=T.float).to(self.policy.device)
+        
+        loss = 0
+        for g, logprob in zip(G, self.action_memory):
+            loss += -g * logprob
+        loss.backward()
+        self.policy.optimizer.step()
+        
+        self.action_memory = []
+        self.reward_memory = []
+        
