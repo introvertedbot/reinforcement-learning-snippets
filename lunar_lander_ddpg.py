be9568592b41dd95e7db3a5340d12cb893c4f3cf
introvertedbot <introvertedbot@protonmail.com>
2021-02-02 04:40:00 +0530

    Finished actor network class

--- lunar_lander_ddpg.py
+++ lunar_lander_ddpg.py
@@ -134,3 +134,67 @@ class CriticNetwork(nn.Module):
         print('... saving best checkpoint ...')
         checkpoint_file = os.path.join(self.checkpoint_dir, self.name+'_best')
         T.save(self.state_dict(), checkpoint_file)
+
+class ActorNetwork(nn.Module):
+    def __init__(self, alpha, input_dims, fc1_dims, fc2_dims, n_actions, name,
+                 chkpt_dir='tmp/ddpg'):
+        super(ActorNetwork, self).__init__()
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
+        self.name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_ddpg')
+
+        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+
+        self.bn1 = nn.LayerNorm(self.fc1_dims)
+        self.bn2 = nn.LayerNorm(self.fc2_dims)
+
+        #self.bn1 = nn.BatchNorm1d(self.fc1_dims)
+        #self.bn2 = nn.BatchNorm1d(self.fc2_dims)
+
+        self.mu = nn.Linear(self.fc2_dims, self.n_actions)
+
+        f2 = 1./np.sqrt(self.fc2.weight.data.size()[0])
+        self.fc2.weight.data.uniform_(-f2, f2)
+        self.fc2.bias.data.uniform_(-f2, f2)
+
+        f1 = 1./np.sqrt(self.fc1.weight.data.size()[0])
+        self.fc1.weight.data.uniform_(-f1, f1)
+        self.fc1.bias.data.uniform_(-f1, f1)
+
+        f3 = 0.003
+        self.mu.weight.data.uniform_(-f3, f3)
+        self.mu.bias.data.uniform_(-f3, f3)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cuda:1')
+
+        self.to(self.device)
+
+    def forward(self, state):
+        x = self.fc1(state)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = self.bn2(x)
+        x = F.relu(x)
+        x = T.tanh(self.mu(x))
+
+        return x
+
+    def save_checkpoint(self):
+        print('... saving checkpoint ...')
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        print('... loading checkpoint ...')
+        self.load_state_dict(T.load(self.checkpoint_file))
+
+    def save_best(self):
+        print('... saving best checkpoint ...')
+        checkpoint_file = os.path.join(self.checkpoint_dir, self.name+'_best')
+        T.save(self.state_dict(), checkpoint_file)
